55180e9bbcaf588356e13550d9b064ee1f9441ec
sunlin <sunlin@kingsoft.com>
2018-11-04 19:57:18 +0800

    初始化

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -15,6 +15,10 @@ sudo docker pull xiaozhi90/nginx:${version}
 # start
 docker_name=nginx
 sudo docker ps -a -q --filter "name=${docker_name}" |grep -q . && docker stop ${docker_name} && docker rm -fv ${docker_name}
-sudo docker run -itd --name ${docker_name} -v /mnt/15t:/mnt/15t --net=host harbor.qyvideo.net/sci-calc/sci-calc-sentinel:$TAG java -jar -Xmx1G -Xmx1G -Dspring.profiles.active=$PROFILE -Dserver.port=9006 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=~/logs/oom -server  -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:~/logs/gc/nlp-api.log sentinel-0.0.1-SNAPSHOT.jar
+sudo docker run -itd --name ${docker_name} -p 80:80 xiaozhi90/nginx:${version} nginx
+
+# -v /mnt/15t:/mnt/15t
+#java -jar -Xmx1G -Xmx1G -Dspring.profiles.active=$PROFILE -Dserver.port=9006 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=~/logs/oom -server  -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:~/logs/gc/nlp-api.log sentinel-0.0.1-SNAPSHOT.jar
+#docker run -itd --name nginx1 -p 80:80
 
 echo "end startup"
