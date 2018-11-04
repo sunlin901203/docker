ba123fee8f5513e9437ff275141458026e705a56
sunlin <sunlin@kingsoft.com>
2018-11-04 19:59:18 +0800

    初始化

--- nginx/install.sh
+++ nginx/install.sh
@@ -7,7 +7,7 @@ echo "nginx image current version is ${version}"
 
 sudo docker build -t xiaozhi90/nginx:${version} -f Dockerfile .
 
-sudo docker login -u xiaozhi90 -p 'xiaozhi90docker'
+sudo docker login -u xiaozhi90 -p '***'
 
 sudo docker tag xiaozhi90/nginx:latest xiaozhi90/nginx:${version}
 
