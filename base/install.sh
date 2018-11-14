1fa0f4868fe2849c30bc7e61338eecf9682913d2
sunlin <sunlin@kingsoft.com>
2018-11-14 19:10:14 +0800

    初始化

---
+++ base/install.sh
@@ -0,0 +1,16 @@
+# 设置输入参数
+application="spring-boot-sample"
+version=$1
+if [ -z "$version" ];then
+    version="latest"
+fi
+
+# 打印参数
+echo "application image current version is ${version}"
+
+# 构建并推送镜像
+sudo docker build -t xiaozhi90/${application}:latest -f Dockerfile .
+sudo docker login -u xiaozhi90 -p 'xiaozhi90docker'
+sudo docker tag xiaozhi90/${application}:latest xiaozhi90/${application}:${version}
+sudo docker push xiaozhi90/${application}:${version}
+# sudo docker push xiaozhi90/${application}:latest
--- java/install.sh
+++ java/install.sh
@@ -1,16 +1,15 @@
-# version
+# 设置输入参数
 version=$1
 if [ -z "$version" ];then
     version="latest"
 fi
-echo "nginx image current version is ${version}"
 
+# 打印参数
+echo "application image current version is ${version}"
+
+# 构建并推送镜像
 sudo docker build -t xiaozhi90/nginx:${version} -f Dockerfile .
-
 sudo docker login -u xiaozhi90 -p 'xiaozhi90docker'
-
 sudo docker tag xiaozhi90/nginx:latest xiaozhi90/nginx:${version}
-
 sudo docker push xiaozhi90/nginx:${version}
-
 sudo docker push xiaozhi90/nginx:latest
