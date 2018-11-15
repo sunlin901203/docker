0e8acce0e6119f32444b197b45909725b4812eb3
sunlin <sunlin@kingsoft.com>
2018-11-15 17:00:25 +0800

    fix

--- nginx-official/startup.sh
+++ nginx-official/startup.sh
@@ -10,6 +10,6 @@ sudo docker pull nginx:${version}
 
 # start
 sudo docker ps -a -q --filter "name=${docker_name}" |grep -q . && docker stop ${docker_name} && docker rm -fv ${docker_name}
-sudo docker run --name ${docker_name} -p 80:80 -v $PWD/www:/www -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/wwwlogs -d nginx
+sudo docker run --name ${docker_name} -p 80:80 -v $PWD/nginx-official/www:/www -v $PWD/nginx-official/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/wwwlogs -d nginx
 
 echo "${docker_name} end startup"
