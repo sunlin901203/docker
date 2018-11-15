9f766debffb68ab4692de3bee32e5f9917911b62
sunlin <sunlin@kingsoft.com>
2018-11-15 16:18:50 +0800

    fix

--- nginx-official/startup.sh
+++ nginx-official/startup.sh
@@ -1,9 +1,5 @@
 docker_name=nginx
-# version
-version=$1
-if [ -z "$version" ];then
-    version="latest"
-fi
+version=stable
 
 echo "${docker_name} begin startup"
 echo "current version is ${version}"
@@ -14,8 +10,6 @@ sudo docker pull nginx:${version}
 
 # start
 sudo docker ps -a -q --filter "name=${docker_name}" |grep -q . && docker stop ${docker_name} && docker rm -fv ${docker_name}
-sudo docker run -itd --name ${docker_name} -p 80:80 xiaozhi90/nginx:${version}
-
 sudo docker run --name ${docker_name} -p 80:80 -v $PWD/www:/www -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/wwwlogs -d nginx
 
 echo "${docker_name} end startup"
