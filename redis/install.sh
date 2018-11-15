7c99f616dce2e2d8125b6de4488a8099068e01ae
sunlin <sunlin@kingsoft.com>
2018-11-15 15:09:54 +0800

    fix

--- redis/install.sh
+++ redis/install.sh
@@ -1,16 +1,16 @@
-# version
+# 设置输入参数
+application="redis"
 version=$1
 if [ -z "$version" ];then
     version="latest"
 fi
-echo "nginx image current version is ${version}"
 
-sudo docker build -t xiaozhi90/nginx:${version} -f Dockerfile .
+# 打印参数
+echo "application image current version is ${version}"
 
+# 构建并推送镜像
+sudo docker build -t xiaozhi90/${application}:latest -f redis/Dockerfile .
 sudo docker login -u xiaozhi90 -p 'xiaozhi90docker'
-
-sudo docker tag xiaozhi90/nginx:latest xiaozhi90/nginx:${version}
-
-sudo docker push xiaozhi90/nginx:${version}
-
-sudo docker push xiaozhi90/nginx:latest
+sudo docker tag xiaozhi90/${application}:latest xiaozhi90/${application}:${version}
+sudo docker push xiaozhi90/${application}:${version}
+sudo docker push xiaozhi90/${application}:latest
--- redis/stop.sh
+++ redis/stop.sh
@@ -1,6 +1,6 @@
 echo "stop start."
 
-application = "spring-boot-sample"
+application = "redis"
 
 # 停止
 sudo docker ps -a -q --filter "name=${application}" |grep -q . && docker stop ${application} && docker rm -fv ${application}
