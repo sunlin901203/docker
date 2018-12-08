d6e527c9ee4c73b4b781b7bc4df6f32d0287b7a3
sunny <sunlin901203@qq.com>
2018-12-09 03:06:53 +0800

    更新

---
+++ other/mac.sh
@@ -0,0 +1,52 @@
+# Mac
+
+cd /opt/tmp
+
+# 日常软件
+wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+
+wget https://a11.gdl.netease.com/NemuInstaller-1.5.4.dmg
+
+wget https://github.com/OpenEmu/OpenEmu/releases/download/v2.0.8/OpenEmu_2.0.8.zip
+
+# 必备软件
+https://download.jetbrains.8686c.com/idea/ideaIU-2018.3.1.dmg
+
+curl -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k "http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz"
+
+wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.13-macos10.14-x86_64.dmg
+
+https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-8.0.13-macos-x86_64.dmg
+
+wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+
+wget https://download.sublimetext.com/Sublime%20Text%20Build%203176.dmg
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.0.tar.gz
+
+# postman
+wget https://dl.pstmn.io/download/latest/osx
+
+wget https://www.charlesproxy.com/assets/release/4.2.7/charles-proxy-4.2.7.dmg
+
+# 其他软件
+https://download.jetbrains.8686c.com/python/pycharm-professional-2018.3.1.dmg
+
+wget https://pkg.jenkins.io/redhat-stable/jenkins-2.150.1-1.1.noarch.rpm
+
+wget https://download.robomongo.org/1.2.1/osx/robo3t-1.2.1-darwin-x86_64-3e50a65.dmg
+
+curl -sSL https://zipkin.io/quickstart.sh | bash -s
+
+
+
+
+
+
+
+
+
+
+
+
+
